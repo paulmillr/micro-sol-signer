b78dd4fd80b107364f8c6b42e2db8ab9e3479253
Paul Miller <paul@paulmillr.com>
2025-05-07 04:49:58 +0000

    More improvements

--- test/index.js
+++ test/index.js
@@ -4,5 +4,7 @@ import './basic.test.js';
 import './versioned.test.js';
 import './idl.test.js';
 import './net.test.js';
+import './offchain.test.js';
+import './tokenmetatada.test.js';
 
 should.runWhen(import.meta.url);
